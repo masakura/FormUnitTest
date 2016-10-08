68be92c8d8f7f82f41646a885aeaeec7c210e9a2
Tomo Masakura <tomo.masakura@gmail.com>
2016-10-08 13:20:49 +0900

    コードに対応していろいろやるように

--- TestDriver/Sub1/AddressSearchServiceStub.cs
+++ TestDriver/Sub1/AddressSearchServiceStub.cs
@@ -9,15 +9,21 @@ namespace TestDriver.Sub1
         public Task<AddressResult> Search(string postalCode)
         {
             if (postalCode == "0000000")
-            {
-                return Task.FromResult(AddressResult.Success("0000000", "鹿児島県", "鹿児島市", "名山町"));
-            }
+                return Address("0000000", "宮崎県", "宮崎市", "宮崎");
             if (postalCode == "0000001")
+                return Address("0000001", "鹿児島県", "鹿児島市", "名山町");
+            if (postalCode == "0000002")
+                return Address("0000002", "とにかくですね", "長い長い長い長い", "住所字とかいろいろはいっててめっちゃながい");
+            if (postalCode == "9999999")
             {
                 throw new InvalidOperationException("サーバーに接続できませんでした");
             }
-
             return Task.FromResult(AddressResult.Empty());
+        }
+
+        private Task<AddressResult> Address(string postalCode, string prefecture, string city, string town)
+        {
+            return Task.FromResult(AddressResult.Success(postalCode, prefecture, city, town));
         }
     }
 }
